7bda74ce9c7dabd8b240124814c2f80c0a7681ec
Christian Persson <saser@live.se>
2016-02-08 16:24:47 +0100

    Replace the example script

--- sql/001-schema.sql
+++ sql/001-schema.sql
@@ -1,4 +1,9 @@
+-- Example of an SQL script
 CREATE TABLE Test (
     id integer,
     PRIMARY KEY(id)
 );
+
+INSERT INTO Test VALUES (1);
+INSERT INTO Test VALUES (2);
+INSERT INTO Test VALUES (3);
