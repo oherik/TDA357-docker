b28bb042d02eb80fee57eaf5c3d52abcefa26045
Erik Öhrn <erik.ohrn@outlook.com>
2016-02-10 16:49:24 +0100

    Added test schema

--- sql/001-schema.sql
+++ sql/001-schema.sql
@@ -7,3 +7,16 @@ CREATE TABLE Test (
 INSERT INTO Test VALUES (1);
 INSERT INTO Test VALUES (2);
 INSERT INTO Test VALUES (3);
+
+CREATE TABLE Countries (
+    name TEXT,
+    capital TEXT,
+    population INT,
+    currency TEXT,
+    PRIMARY KEY (name)
+);
+
+INSERT INTO Countries VALUES ('Sweden','Stockholm',10000000,'SEK');
+INSERT INTO Countries VALUES ('Norway','Oslo',4500000,'NOK');
+
+SELECT * FROM Countries;
